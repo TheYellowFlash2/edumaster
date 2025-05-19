ba8e0732daf59f9fb81a77db77a128a59ed94cac
TheYellowFlash2 <phillipwrencher5@gmail.com>
2025-05-19 08:32:39 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export  const firebaseConfig = {
+    apiKey: "AIzaSyD31k6XKHpftf-IqLEgfMvtAcI8OQyBoqI",
+    authDomain: "school-test-91012.firebaseapp.com",
+    projectId: "school-test-91012",
+    storageBucket: "school-test-91012.firebasestorage.app",
+    messagingSenderId: "906695439647",
+    appId: "1:906695439647:web:f19c5369600e319a30ec2a",
+    measurementId: "G-Y15HXKENSD"
+  };
+
